10bf7df9dcdcdb9e0faecfb7231bab9c905331d1
LetMyPplGo <54865472+LetMyPplGo@users.noreply.github.com>
2020-05-03 00:52:44 +0300

    separated test code from the rest

--- test1.py
+++ test1.py
@@ -4,63 +4,73 @@ from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 from testrail import *
 import time
 import inspect
+from traceback import format_tb
+import sys
 
 
-def testcase(func):
-    def wrapper(*args, **kwargs):
+def test_case_web(func):
+    def wrapper(capabilities, *args, **kwargs):
         try:
-            return func(*args, **kwargs)
+            capabilities["name"] = func.__name__
+
+            if capabilities["enableVideo"]:
+                video_name = func.__name__ + "." + capabilities["browserName"] + "." + time.strftime("%Y%m%d-%H%M%S") + ".mp4"
+                capabilities["videoName"] = video_name
+
+            if capabilities["enableLog"]:
+                log_name = func.__name__ + "." + capabilities["browserName"] + "." + time.strftime("%Y%m%d-%H%M%S") + ".log"
+                capabilities["logName"] = log_name
+
+            # Selenoid should be started in advance with:
+            # > cm.exe selenoid start --vnc
+            driver = webdriver.Remote(command_executor="http://localhost:4444/wd/hub",
+                                      desired_capabilities=capabilities)
+
+            func(driver, *args, **kwargs)
         except AssertionError as err:
-            # if function creates an archive to attach to fail collect it here and return as third parameter
-            # the archive can be named by the function name func.__name__
-            return R_FAIL, "{}".format(err)
+            # TODO: if function creates an archive to attach to fail collect it here and return as third parameter
+            #       the archive can be named by the function name func.__name__
+            if str(err) != "":
+                return R_FAIL, f"{err}"
+            else:
+                lines = format_tb(err.__traceback__)
+                return R_FAIL, lines[-1].split("\n")[1]
+        else:
+            return R_PASS, ""
     return wrapper
 
 
 class TestFactory:
 
     @staticmethod
-    @testcase
-    def test_ma_values(capabilities):
-        # run with selenium on locally installed browser
-        # driver = webdriver.Opera()
-
-        # this capcbility doesn't work for some reason...
-        if capabilities["enableVideo"]:
-            my_name = inspect.currentframe().f_code.co_name
-            video_name = my_name + "." + capabilities["browserName"] + ".mp4"
-            capabilities["videoName"] = video_name
-
-        # run on selenoid with browsers running in Docker
-        driver = webdriver.Remote(command_executor="http://localhost:4444/wd/hub",
-                                  desired_capabilities=capabilities)
-
-        # the test itself
+    @test_case_web
+    def test_ma_values(driver):
         driver.get("http://python.org")
         assert "Python" in driver.title, "python is not in the title"
         elem = driver.find_element_by_name("q")
         elem.clear()
         elem.send_keys("pycon" + Keys.RETURN)
-        assert "No results found." in driver.page_source, "Expected: 'No results found' on the page"
+        assert "No results found." in driver.page_source
         driver.close()
-        return R_PASS, ""
 
 
 def test_run(run_id):
-    """iterates the given testrail Test Run and executes each test"""
+    """iterates the given Test Run and executes each test"""
     tests = testrail_client.send_get('get_tests/' + run_id)
     for test in tests:
         test_id = test['id']
         test_name = test['custom_autotest_name']
         if test_name is not None:
-            print("Test name: {}, autotest: {}".format(test['title'], test_name))
+            print(f"Test name: {test['title']}, autotest: {test_name}")
             assert hasattr(TestFactory, test_name)
             time1 = time.time()
             # result, comment = TestFactory.test_ma_values()
             result, comment = getattr(TestFactory, test_name)()
             elapsed = str(round(time.time() - time1)) + "s"
-            print("Result: {}, {}, spent {}".format(results[result], comment, elapsed))
-            testrail_client.send_post('add_result/{}'.format(test_id), {'status_id': result, 'comment': comment, 'elapsed': elapsed})
+            print(f"Result: {results[result]}, {comment}, spent {elapsed}")
+            testrail_client.send_post(f'add_result/{test_id}',
+                                      {'status_id': result, 'comment': comment, 'elapsed': elapsed}
+                                      )
 
 
 def get_test_runs():
@@ -72,7 +82,8 @@ def get_test_runs():
 def default_capabilities(browser):
     return {"browserName": browser,
             "enableVNC": True,
-            "enableVideo": True
+            "enableVideo": False,
+            "enableLog": False
             }
 
 
@@ -100,19 +111,13 @@ result, comment = TestFactory.test_ma_values(CAP_CHROME)
 elapsed = str(round(time.time() - time1)) + "s"
 print("Result: {}, {}, spent {}".format(results[result], comment, elapsed))
 
-time1 = time.time()
-result, comment = TestFactory.test_ma_values(CAP_OPERA)
-elapsed = str(round(time.time() - time1)) + "s"
-print("Result: {}, {}, spent {}".format(results[result], comment, elapsed))
+# time1 = time.time()
+# result, comment = TestFactory.test_ma_values(CAP_OPERA)
+# elapsed = str(round(time.time() - time1)) + "s"
+# print("Result: {}, {}, spent {}".format(results[result], comment, elapsed))
+#
+# time1 = time.time()
+# result, comment = TestFactory.test_ma_values(CAP_FIREFOX)
+# elapsed = str(round(time.time() - time1)) + "s"
+# print("Result: {}, {}, spent {}".format(results[result], comment, elapsed))
 
-time1 = time.time()
-result, comment = TestFactory.test_ma_values(CAP_FIREFOX)
-elapsed = str(round(time.time() - time1)) + "s"
-print("Result: {}, {}, spent {}".format(results[result], comment, elapsed))
-
-
-# driver = webdriver.Remote(command_executor="http://localhost:4444/wd/hub",
-#                           desired_capabilities={
-#                               "browserName": "chrome",
-#                           })
-# print(driver.capabilities)
